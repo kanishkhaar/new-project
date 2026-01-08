9c94318569b9998439c41412f889ab45b5f4d578
Kanishkha A R <kanishkha57@gmail.com>
2026-01-08 16:03:48 +0530

    Update app.py

--- podcast/app.py
+++ podcast/app.py
@@ -5,7 +5,6 @@ import nltk
 nltk.download("punkt")
 nltk.download("punkt_tab")
 from nltk.tokenize import sent_tokenize
-
 import whisper
 from jiwer import wer
 from sentence_transformers import SentenceTransformer
@@ -15,41 +14,30 @@ print("Audio to Text...")
 model = whisper.load_model("base")
 result = model.transcribe("podcast.wav")
 transcript = result["text"]
-
 print("\nTRANSCRIPT:\n", transcript)
-
 reference_text = transcript
 error = wer(reference_text, transcript)
 accuracy = (1 - error) * 100
-
 print("\nACCURACY:")
 print("WER:", error)
 print("Accuracy:", accuracy, "%")
-
-# 3️⃣ TOPIC SEGMENTATION
 print("\nTOPIC SEGMENTS:")
-
 sentences = sent_tokenize(transcript)
 model = SentenceTransformer("all-MiniLM-L6-v2")
 embeddings = model.encode(sentences)
-
 threshold = 0.6
 current = [sentences[0]]
 segments = []
-
 for i in range(1, len(sentences)):
     sim = cosine_similarity(
         [embeddings[i-1]],
         [embeddings[i]]
-    )[0][0]
-
+    )[0][0
     if sim < threshold:
         segments.append(" ".join(current))
         current = [sentences[i]]
     else:
         current.append(sentences[i])
-
 segments.append(" ".join(current))
-
 for i, seg in enumerate(segments):
     print(f"\nTopic {i+1}:\n{seg}")
